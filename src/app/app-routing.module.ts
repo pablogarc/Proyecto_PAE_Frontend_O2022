2e685c0d4da6fa7f397fc3a5be40009a12f22056
pablogarc <juan.garo29si@gmail.com>
2022-10-17 23:09:53 -0500

    Setup front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { WatchListComponent } from './pages/profile/watch-list/watch-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'movies/:title', component: MovieDetailsComponent },
+  { path: 'profile/:id', component: ProfileComponent },
+  { path: 'profile/:id/watchlist', component: WatchListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
+import { WatchListComponent } from './pages/profile/watch-list/watch-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent,
+    MoviesComponent,
+    MovieDetailsComponent,
+    WatchListComponent
   ],
   imports: [
     BrowserModule,
